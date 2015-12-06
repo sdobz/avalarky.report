71de475f13fcff90480844c3d58187002f2c5ac5
Vincent Khougaz <vincent@wearehathway.com>
2015-12-05 17:50:56 -0800

    Renamed marshall to marshal, moved theme to content (as it should be), added bootstrap file

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
-#!./marshall/env/bin/python
+#!./.env/bin/python
 
 import os
 import sys
 sys.path.append(os.path.dirname(__file__))
 
-from kyre.settings import load_settings
-settings = load_settings(['avalarky.report.kyre', 'avalarky.report.secret.kyre'])
+import json
 
-from marshall.pelican.horrible_monkey_patch import patch_pelican_settings
+with open('avalarky.report.kyre') as f:
+    settings = json.load(f)
+
+with open('avalarky.report.secret.kyre') as f:
+    settings.update(json.load(f))
+
+from marshal.pelican.horrible_monkey_patch import patch_pelican_settings
 pelican_settings = patch_pelican_settings(settings['pelican'])
 
-from marshall.everdown import run as everdown_run
+from marshal.everdown import run as everdown_run
 everdown_run(settings['everdown'], pelican_settings)
 
 from pelican import main
