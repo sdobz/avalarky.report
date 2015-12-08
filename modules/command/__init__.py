77b4a79f58f33d9a234bf63a2e60b4c66c1d0e6e
Vincent Khougaz <vincent@wearehathway.com>
2015-12-08 03:51:33 -0800

    Simplified command module because execute is now more generic

--- modules/command/__init__.py
+++ modules/command/__init__.py
@@ -1,11 +1,6 @@
 from ..kyre import execute
 import sys
 
-
-def execute_list(execution_list):
-    for execution in execution_list:
-        execute(execution)
-
 defined_commands = {}
 
 
@@ -13,11 +8,11 @@ def add(name, run):
     global defined_commands
     defined_commands[name] = run
     if name in sys.argv:
-        execute_list(run)
+        execute(run)
 
 
-def run(commands):
+def run(*commands):
     global defined_commands
     for command in commands:
         if command in defined_commands:
-            execute_list(defined_commands[command])
+            execute(defined_commands[command])
